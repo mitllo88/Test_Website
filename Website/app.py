018aa66ae74051222d788fb33447b13b62bcc5ad
Mitchell Lloyd <lloyma1@student.op.ac.nz>
2020-03-18 09:32:50 +1300

    added api stuff

--- Website/app.py
+++ Website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from mongoengine import *
 import json
 
@@ -12,9 +12,6 @@ class Country(Document):
 @app.route('/home')
 @app.route('/Home')
 def index():
-	Country(name="New Zealand").save()
-	Country(name="Australia").save()
-	Country(name="Japan").save()
 	return render_template('index.html')
 	
 @app.route('/inspirations')
@@ -22,13 +19,32 @@ def index():
 def inspiration():
 	return render_template('page_1.html')
 	
+@app.route('/about')
+def about():
+	return render_template('page_2.html')	
+
 @app.route('/loadData')
 def load():
-	return 'Loaded!'
+	Country(name="New Zealand").save()
+	Country(name="Australia").save()
+	Country(name="Japan").save()
 	
-@app.route('/listCountriesTest')
-def listCountriesTest():
-	return Country.objects.to_json()
+@app.route('/countries', methods=['GET'])
+@app.route('/countries/<country_name>', methods=['GET'])
+def getCountry(country_name=None):
+	countries = None
+	if country_name is None:
+		return Country.objects.to_json()
+	else:
+		return Country.objects.get(name=country_name).to_json()
 	
+
+@app.route('/countries', methods=['POST'])
+def addCountry():
+	name = request.json['name']
+	new_country = Country(name)
+	new_country.save()
+	return jsonify(new_country)
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80)
+	app.run(debug=True, port=8080)
